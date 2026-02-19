b97c8bddfed107e4852d3734abe17a70a5e64599
Nishan-k <karki.nishann@gmail.com>
2026-02-19 12:02:55 +0100

    Update notebook as .html

--- notebooks/02_.bat
+++ notebooks/02_.bat
@@ -1,5 +1,5 @@
 @REM Convert notebook to HTML and save in html_files folder
-jupyter nbconvert --to html "04_Regional_Performance_with_Growth_Trends.ipynb" --output-dir "../docs"
+jupyter nbconvert --to html "02_Revenue Leakage Analysis.ipynb" --output-dir "../docs"
 
 
 @REM Go to the root of the project to git add everything
